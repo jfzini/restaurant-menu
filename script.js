82daedb8c32b3ec386183c5718eff1e4cd3475e3
João Felipe Zini <joaofelipezini@gmail.com>
2023-02-21 19:22:50 -0300

    adicionando chevrown up and down

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const sectionMenu = document.querySelectorAll('.section-menu');
 const sectionHead = document.querySelectorAll('h2');
+const arrows = document.querySelectorAll('.arrow-icon');
+const sectionNames = document.querySelectorAll('.section-name');
 
 window.onload = () => {
-  for (const h2 of sectionHead) {
-    h2.addEventListener('click', () => {
-      const next = h2.nextElementSibling.style;
+  for (const section of sectionNames) {
+    section.addEventListener('click', () => {
+      const next = section.nextElementSibling.style;
       if (next.display === '') {
         next.display = 'flex';
-        h2.nextElementSibling.classList.add('show');
+        section.nextElementSibling.classList.add('show');
+        section.lastElementChild.src = 'img/up-arrows.png';
       } else {
         next.display = '';
+        section.lastElementChild.src = 'img/down-arrows.png';
       }
     })
   }
